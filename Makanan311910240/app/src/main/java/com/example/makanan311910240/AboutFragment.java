4e7952bf67863c21c4049ff53dc583c9f4fcbc5a
Alim311 <abdualim289@gmail.com>
2022-07-14 14:13:04 +0700

    Regina Febrianti

---
+++ Makanan311910240/app/src/main/java/com/example/makanan311910240/AboutFragment.java
@@ -0,0 +1,27 @@
+package com.example.makanan311910240;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+
+public class AboutFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View aboutView = inflater.inflate(R.layout.fragment_about,
+                container, false);
+
+        WebView webView = aboutView.findViewById(R.id.wv_about);
+        webView.loadUrl("file:///android_asset/index.html");
+        // Inflate the layout for this fragment
+        return aboutView;
+
+    }
+}
---
+++ Makanan311910240/app/src/main/java/com/example/makanan311910240/ModelKuliner.java
@@ -0,0 +1,32 @@
+package com.example.makanan311910240;
+
+public class ModelKuliner {
+
+    private String nama;
+    private String tentang;
+    private String foto;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getTentang() {
+        return tentang;
+    }
+
+    public void setTentang(String tentang) {
+        this.tentang = tentang;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
